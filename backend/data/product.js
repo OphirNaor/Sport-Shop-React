8ea1babf4b16614134c01e7911f0f2c2e34f6e3f
OphirNaor <ophir1991@gmail.com>
2022-05-17 19:22:54 +0300

    sport shop app

---
+++ backend/data/product.js
@@ -0,0 +1,85 @@
+const products = [
+    {
+        name: "Kettlebells",
+        imageUrl:
+            "https://static-content-3.boomfit.com/7789-large_default/zwarte-kettlebell-cross-training-20kg.jpg",
+        description:
+            "Kettlebells focus on improving the overall strength, core power, balance, flexibility, and coordination of your body.",
+        price: 20,
+        countInStock: 15,
+    },
+    {
+        name: "Dumbbells",
+        imageUrl:
+            "https://www.tradeinn.com/f/13796/137961908/gymstick-hex-dumbbell-6-kg-unit.jpg",
+        description:
+            "Dumbbells help build and tone muscles so that you can show off your efforts to all those around you",
+        price: 15,
+        countInStock: 10,
+    },
+    {
+        name: "Barbells",
+        imageUrl:
+            "https://helisports.pictures/products/1000/hastings/professional-barbells/40kg-1/hastings-professional-barbell-40kg.jpg",
+        description:
+            "Barbells are great tools for any fitness level and can be used for practically any form of lifting.",
+        price: 50,
+        countInStock: 15,
+    },
+    {
+        name: "Treadmills",
+        imageUrl:
+            "https://www.nautilus.com/dw/image/v2/AAYW_PRD/on/demandware.static/-/Sites-nautilus-master-catalog/default/dw83947c0c/images/nautilus/treadmills/t618/100647/nautilus-treadmill-t618-1.jpg?sw=700&sh=700&sm=fit",
+        description:
+            "Treadmill workouts remain a staple of exercise routines because they are a terrific way to burn calories.",
+        price: 150,
+        countInStock: 7,
+    },
+    {
+        name: "TRX",
+        imageUrl:
+            "https://m.media-amazon.com/images/I/71+jQvFoDAL._AC_SX425_.jpg",
+        description:
+            "TRX can improve mobility and stability, increase metabolic results, build lean muscle, and develop functional strength.",
+        price: 25,
+        countInStock: 14,
+    },
+    {
+        name: "Bicycles",
+        imageUrl:
+            "https://ik.imagekit.io/solefitness/WebImages/tr:w-700,q-100/SB700-2021-Front-Right.png",
+        description:
+            "Cycling improves cardiovascular health, helping with weight loss and improved posture and coordination.",
+        price: 140,
+        countInStock: 10,
+    },
+    {
+        name: "Skipping Ropes",
+        imageUrl:
+            "https://m.media-amazon.com/images/I/61KA01XxDML._AC_SY450_.jpg",
+        description:
+            "Skipping ropes are affordable, lightweight and seriously effective.",
+        price: 10,
+        countInStock: 20,
+    },
+    {
+        name: "Boxing Bag",
+        imageUrl:
+            "https://cdn.shopify.com/s/files/1/1055/5166/products/SHOPIFYNevatear3FootPunchingBag_2400x.png?v=1637307101",
+        description:
+            "Boxing bags are cylindrical bags built to be hit repeatedly. They are filled with different materials of varying hardness.",
+        price: 125,
+        countInStock: 13,
+    },
+    {
+        name: "Pullups Bar",
+        imageUrl:
+            "https://cdn.shopify.com/s/files/1/0268/5678/3943/products/5557_1500x.jpg?v=1574976018",
+        description:
+            "Pull-ups strengthen and sculpt your back, shoulders, forearms",
+        price: 115,
+        countInStock: 23,
+    },
+];
+
+module.exports = products;
